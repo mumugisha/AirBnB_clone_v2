29861d7ec881b20766e45efd97125f1b94f639c2
mumugisha <prudent.nkubito@gmail.com>
2024-07-28 22:13:34 +0300

    AirBnB clone - Web framework

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,13 +1,1 @@
-#!/usr/bin/python3
-
-from flask import Flask
-
-def create_app():
-        app = Flask(__name__)
-
-            @app.route('/', strict_slashes=False)
-                def hello_hbnb():
-                            return "Hello HBNB!"
-
-                            return app
 

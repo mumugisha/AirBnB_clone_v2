0afa099492c7aaa06dc905f869dcda6046ca064e
mumugisha <prudent.nkubito@gmail.com>
2024-07-28 21:32:08 +0300

    AirBnB clone - Web framework

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+        app = Flask(__name__)
+
+            @app.route('/', strict_slashes=False)
+                def hello_hbnb():
+                            return "Hello HBNB!"
+
+                            return app
---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application.
+The web application must be listening on 0.0.0.0, port 5000.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+        """Open route."""
+        return "Hello HBNB!"
+
+        if __name__ == '__main__':
+                app.run(host='0.0.0.0', port=5000)

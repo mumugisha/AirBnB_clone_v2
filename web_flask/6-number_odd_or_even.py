a96c75a68e08b4e916c212b15102c7f09d5a8a0f
mumugisha <prudent.nkubito@gmail.com>
2024-07-28 17:25:15 -0700

    AirBnB clone - Web framework

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+The web application listens on 0.0.0.0, port 5000.
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Handle requests to the root URL '/'.
+    Returns a simple greeting message.
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Handle requests to the '/hbnb' URL.
+    Returns a different greeting message.
+    """
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    Handle requests to the '/c/<text>' URL.
+    Replaces underscores with spaces in the text.
+    """
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """
+    Handle requests to the '/python/<text>' URL.
+    Replaces underscores with spaces in the text.
+    """
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Handle requests to the '/number/<int:n>' URL.
+    Displays “n is a number” only if n is an integer.
+    """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def template(n):
+    """
+    Handle template requests to the 'number_template/<n>' URL.
+    Display an HTML page only if n is an integer.
+    """
+    return render_template("5-template.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_even(n):
+    """
+    Handle template requests to the 'number_odd_or_even/<n>' URL.
+    display a HTML page only if n is an integer.
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == '__main__':
+    # Run the Flask application on all available IP addresses and port 5000.
+    app.run(host='0.0.0.0', port=5000)
+
